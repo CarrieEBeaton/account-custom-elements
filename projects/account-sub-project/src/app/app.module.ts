501d5b84138767df016b25232d34107ff186de6d
Carrie BEATON <carrie.beaton@amadeus.com>
2018-12-02 09:39:02 -0500

    initial commit

--- projects/account-sub-project/src/app/app.module.ts
+++ projects/account-sub-project/src/app/app.module.ts
@@ -3,23 +3,24 @@ import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { AccountModule } from 'projects/account-sub-project/src/app/account/account.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { AccountData } from 'projects/account-sub-project/src/app/account/account-data';
-// tslint:disable-next-line:max-line-length
-import { AccountContainerComponent } from 'projects/account-sub-project/src/app/account/containers/account-container/account-container.component';
+import { AccountData } from './account/account-data';
+import { AccountModule } from './account/account.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { createCustomElement } from '@angular/elements';
+import { AccountContainerComponent } from './account/containers/account-container/account-container.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AccountModule,
+    AppRoutingModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     HttpClientModule,
@@ -28,8 +29,7 @@ import { createCustomElement } from '@angular/elements';
   providers: [],
   entryComponents: [
     AccountContainerComponent
-  ],
-  bootstrap: []
+]
 })
 export class AppModule {
   constructor(private injector: Injector) {
