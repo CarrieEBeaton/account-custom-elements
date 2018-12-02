4aa41d00a96df10bbe249553f781f5dbc3f13f4d
Carrie BEATON <carrie.beaton@amadeus.com>
2018-12-02 11:54:30 -0500

    registered angular element in app module and an loading from there insteal of index.html

--- projects/account-sub-project/src/app/app.module.ts
+++ projects/account-sub-project/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, Injector } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AccountData } from './account/account-data';
 import { AccountModule } from './account/account.module';
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { createCustomElement } from '@angular/elements';
 import { AccountContainerComponent } from './account/containers/account-container/account-container.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AccountModule,
-    AppRoutingModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     HttpClientModule,
@@ -29,14 +23,14 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [],
   entryComponents: [
     AccountContainerComponent
-]
+  ]
 })
 export class AppModule {
   constructor(private injector: Injector) {
   }
 
   ngDoBootstrap() {
-      const accountContainer = createCustomElement(AccountContainerComponent, { injector: this.injector });
-      customElements.define('account-container', accountContainer);
+    const accountContainer = createCustomElement(AccountContainerComponent, { injector: this.injector });
+    customElements.define('account-container', accountContainer);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, Injector } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { AccountModule } from 'projects/account-sub-project/src/app/account/account.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AccountData } from 'projects/account-sub-project/src/app/account/account-data';
+import { AccountModule } from 'projects/account-sub-project/src/app/account/account.module';
 // tslint:disable-next-line:max-line-length
 import { AccountContainerComponent } from 'projects/account-sub-project/src/app/account/containers/account-container/account-container.component';
-import { createCustomElement } from '@angular/elements';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { createCustomElement } from '@angular/elements';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AccountModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
@@ -29,14 +27,15 @@ import { createCustomElement } from '@angular/elements';
   entryComponents: [
     AccountContainerComponent
   ],
-  bootstrap: []
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor(private injector: Injector) {
+    const accountContainer = createCustomElement(AccountContainerComponent, { injector: this.injector });
+    customElements.define('account-container', accountContainer);
   }
 
   ngDoBootstrap() {
-      const accountContainer = createCustomElement(AccountContainerComponent, { injector: this.injector });
-      customElements.define('account-container', accountContainer);
   }
 }
