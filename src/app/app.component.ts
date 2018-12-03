45965540a27741190726ae7d14737b218782136e
Carrie BEATON <carrie.beaton@amadeus.com>
2018-12-02 21:57:21 -0500

    added a way to load content dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'account-custom-elements';
+
+  addExternal() {
+    const script = document.createElement('script');
+    script.src = 'assets/account-sub-project.bundle.js';
+    document.body.appendChild(script);
+
+    // add web component
+    const tile = document.createElement('account-container');
+    tile.setAttribute('a', '100');
+
+    const content = document.getElementById('content');
+    content.appendChild(tile);
+
+  }
 }
